74887c8381ec6e9746e5ab6e4324b82560404d09
Baulin_Roman_Sergeevi4 <poma.baulin@mail.ru>
2022-06-24 13:38:10 +0300

    add seminar2

---
+++ Seminar2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите первое число :");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число :");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число :");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+ if(num1 > num2)
+{
+    if(num1>num3)
+    {
+        Console.WriteLine($"Число мах: {num1}");  
+    }
+}
+else if(num2 > num3)
+{
+    Console.WriteLine($"Число мах: {num2}");
+}
+else
+{
+    Console.WriteLine($"Число мах: {num3}");
+}    
