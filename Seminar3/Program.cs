5091644a3cc4bfb7bc4f59560b5395ca4f6b220c
Baulin_Roman_Sergeevi4 <poma.baulin@mail.ru>
2022-06-24 18:17:12 +0300

    add Seminar3new add

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число для проверки");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else 
+{
+    Console.WriteLine("Число нечетное");
+    
+}
