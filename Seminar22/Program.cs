f1788d8229d4c9222c47fcc22c8478c7676e06ab
Baulin_Roman_Sergeevi4 <poma.baulin@mail.ru>
2022-06-27 13:16:27 +0300

    add seminar22

---
+++ Seminar22/Program.cs
@@ -0,0 +1,12 @@
+﻿
+//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число:");
+int threenumber = Convert.ToInt32(Console.ReadLine());
+
+//int num1 = threenumber / 10;
+//int sum = num1 % 10;
+int sum = (threenumber/10)%10;  // Сокращение. 
+
+Console.WriteLine($"Второе число: {sum}");
+
